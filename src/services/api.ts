765d9b90ca4713a8b6bb93db4c7a15c15f305c6e
Lucas da SIlva Freitas <lucasdasilvafreitas090@gmail.com>
2022-06-04 00:09:58 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const config = {
-  apiKey: "AIzaSyB6tx2uKQH86lgXdUem0PmaYQOYXjWNuZA",
-  authDomain: "bazar-cd12e.firebaseapp.com",
-  projectId: "bazar-cd12e",
-  storageBucket: "bazar-cd12e.appspot.com",
-  messagingSenderId: "527020146391",
-  appId: "1:527020146391:web:b5e00c786c36d58c4f3a92",
-};
+const config = {};
 
 const app = initializeApp(config);
 export const db = getFirestore(app);
